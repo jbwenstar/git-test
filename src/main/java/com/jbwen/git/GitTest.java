16735f1aec22a74163e24475e5ccd76c7f2e1eac
jbwen <1529863413@qq.com>
2022-05-02 20:33:55 +0800

    idea-v2+

--- src/main/java/com/jbwen/git/GitTest.java
+++ src/main/java/com/jbwen/git/GitTest.java
@@ -8,6 +8,6 @@ package com.jbwen.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git !");
-        System.out.println("+++");
+        System.out.println("+by hot+");
     }
 }
