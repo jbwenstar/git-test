94e5c9ce0eb2558b97fd9be2c430928c5f92dc93
jbwen <1529863413@qq.com>
2022-05-02 20:27:18 +0800

    idea-v1

---
+++ src/main/java/com/jbwen/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.jbwen.git;
+
+/**
+ * @Desccription:TODO(说明作用)
+ * @Author wjbin jun
+ * @Date 2022/5/2 20:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+    }
+}
